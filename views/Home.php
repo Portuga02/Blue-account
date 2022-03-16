c26d532ee56817fa20f005f6839692514ee7dc04
Sávio Gomes da Silva <saviogomesdasilva@outlook.com.br>
2022-03-16 19:53:29 -0300

    ajustando menu

--- views/Home.php
+++ views/Home.php
@@ -260,16 +260,16 @@
                                 <a href="#" class="nav-link">
                                     <i class="nav-icon fas fa-copy"></i>
                                     <p>
-                                        Layout Options
+                                       Home 
                                         <i class="fas fa-angle-left right"></i>
                                         <span class="badge badge-info right">6</span>
                                     </p>
                                 </a>
                                 <ul class="nav nav-treeview">
                                     <li class="nav-item">
-                                        <a href="pages/layout/top-nav.html" class="nav-link">
+                                        <a href="" class="nav-link">
                                             <i class="far fa-circle nav-icon"></i>
-                                            <p>Top Navigation</p>
+                                            <p>Permissões</p>
                                         </a>
                                     </li>
                                     <li class="nav-item">
