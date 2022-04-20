b948828eb7c708b029d27e46d344ed48ead3a969
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2022-04-20 09:16:38 -0300

    AJUSTANDO PERMISSOES

--- views/Permissions.php
+++ views/Permissions.php
@@ -25,8 +25,8 @@
                     <div class="card-header d-flex p-0">
                         <h3 class="card-title p-3">Tabs</h3>
                         <ul class="nav nav-pills ml-auto p-2">
-                            <li class="nav-item"><a class="nav-link active" href="#tab_1" data-toggle="tab">Permissões</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#tab_2" data-toggle="tab">Grupos</a></li>
+                            <li class="nav-item"><a class="nav-link active" href="#tab_1" data-toggle="tab">Grupo de permissões</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#tab_2" data-toggle="tab">Permissões</a></li>
                         </ul>
                     </div><!-- /.card-header -->
                     <div class="card-body">
